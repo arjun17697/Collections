ab8acbb31ebf9cad1cc7004fafa5823c0345fb9c
arjun17697 <arjun17697@gmail.com>
2020-10-01 16:53:22 +0530

    Adding UC6

---
+++ com/bridgelabz/addressbook/UC6_AddMultipleAddBook.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.addressbook;
+import java.util.*;
+public class UC6_AddMultipleAddBook {
+	public static void main(String[] args) {
+		Scanner stdin = new Scanner(System.in);
+		System.out.println("What is the number of address books you want to add?");
+		int addnum = stdin.nextInt();
+
+		
+    for (int i = 0; i < addnum; i++) {
+    }
+    	// Instantiate AddressBook object
+		AddressBook name = new AddressBook();
+    }
+	}
