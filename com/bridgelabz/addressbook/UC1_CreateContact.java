d5dc694833b59ed847567978183c79e42c0969f4
arjun17697 <arjun17697@gmail.com>
2020-10-01 12:38:35 +0530

    Adding UC1_CreateContact

---
+++ com/bridgelabz/addressbook/UC1_CreateContact.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.addressbook;
+
+import java.util.*;
+
+public class UC1_CreateContact {
+	public static void main(String[] args) {
+
+			Scanner stdin = new Scanner(System.in);
+
+			//Reads in all appropriate information.");
+    		System.out.println("Enter your friend's first name:");
+    		String fname = stdin.next();
+    		System.out.println("Enter your friend's last name:");
+    		String lname = stdin.next();
+    		System.out.println("Enter his phone number:");
+    		String phonenumber = stdin.next();
+    		System.out.println("Enter his city");
+    		String city = stdin.next();
+    		System.out.println("Enter his address");
+    		String address = stdin.next();
+    		System.out.println("Enter his state");
+    		String state = stdin.next();
+    		System.out.println("Enter his zip code");
+    		int zip = stdin.nextInt();
+    		System.out.println("Enter his email");
+    		String email = stdin.next();
+
+    		
+		}
+
+
+}
